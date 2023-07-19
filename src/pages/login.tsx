4408b0cd2c315e143a6bdd7319be519c8d005633
Sayantan <sayantan358@gmail.com>
2023-07-19 21:28:05 +0530

    log in page updated

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,39 @@
 import Head from "next/head";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface Inputs {
+  email: String;
+  password: String;
+}
 
 const login = () => {
+  const [login, setLogin] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async({ email, password}) => {
+    if (login) {
+      // await signIn(email, password)
+    }
+    else {
+      // await signUp(email, password)
+    }
+  };
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
         <title>ScreenWave</title>
       </Head>
       <Image
-        src="https://images.unsplash.com/photo-1643208589889-0735ad7218f0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+        src="https://images.unsplash.com/photo-1593627829758-a5969d9124b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
         alt="Login Screen Image"
         fill
-        className="-z-10 !hidden opacity-60 sm:!inline object-cover"
+        className="-z-10 !hidden opacity-60 sm:!inline"
       />
 
       <Image
@@ -24,15 +45,56 @@ const login = () => {
         priority={true}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
-        <h1>Sign In</h1>
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="text-4xl">Sign In</h1>
         <div className="space-y-4">
-          <label htmlFor="">
-            <input type="email" name="" id="" placeholder="Email"/>
+          <label htmlFor="" className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
-          <label htmlFor="">
-          <input type="password" name="" id="" placeholder="Password"/>
+          <label htmlFor="" className="inline-block w-full">
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
+        </div>
+
+        <button
+          className="w-full rounded bg-[#E50914] py-3 font-semibold hover:bg-[#a7181f] transition-all duration-200"
+          onClick={() => setLogin(true)}
+        >
+          Sign In
+        </button>
+
+        <div className="text-[gray]">
+          New to Netflix?{" "}
+          <button
+            type="submit"
+            className="text-white hover:underline"
+            onClick={() => setLogin(false)}
+          >
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
